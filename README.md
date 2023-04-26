145c76a3fb1330d14701ebd03a11ebae7fc9fb09
k1ut3h <hardikedu811@gmail.com>
2023-04-26 11:44:05 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chat server in Rust.
+- Made this project to learn tokio.
+---
+## Here's the [tutorial](https://www.youtube.com/watch?v=Iapc-qGTEBQ&pp=ygUOdG9raW8gdHV0b3JpYWw%3D)
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use tokio::net::{TcpListener};
+use tokio::io::{AsyncWriteExt, BufReader,AsyncBufReadExt};
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    let (tx, _) = broadcast::channel(10);
+
+    loop{
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move{
+            let (read, mut write) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop{
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap()==0{
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr{
+                            write.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
